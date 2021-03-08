7421a6096c5246cdc9e8d29964236741bc89c061
KybenNabor <79893978+kybenabor@users.noreply.github.com>
2021-03-08 18:23:08 +0800

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,13 @@
+# SoftwareDesign
+public class main {
+	String firstname = "Kyben";
+	String lastname = "Nabor";
+
+public static void main (string[] args) {
+	main myObj = new Main();
+	System.out.println("First Name:" + myObj.firstname);
+	System.out.println("Last Name:" + myObj.lastname);
+   
+   }
+ }
+	
