d64d5405e331ae472b935b5e7d6bc0e498edc3d4
KybenNabor <79893978+kybenabor@users.noreply.github.com>
2021-03-08 19:03:51 +0800

    Create Main.java
    5 Person

---
+++ Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+  private String firstname1 = "Steven";
+  private String lastname1 = "Seagull";
+  private String firstname2 = "Ryan";
+  private String lastname2 = "Bang";
+  private String firstname3 = "Nyear";
+  private String lastname3 = "Samson";
+  private String firstname4 = "Paul";
+  private String lastname4= "Phoenix";
+  private String firstname5 = "Jim";
+  private String lastname5= "Carter";
+  
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    System.out.println("Name: " + myObj.firstname1 + " " + myObj.lastname1);
+    System.out.println("Name: " + myObj.firstname2 + " " + myObj.lastname2);
+    System.out.println("Name: " + myObj.firstname3 + " " + myObj.lastname3);
+    System.out.println("Name: " + myObj.firstname4 + " " + myObj.lastname4);
+    System.out.println("Name: " + myObj.firstname5 + " " + myObj.lastname5);
+    
+  }
+}
