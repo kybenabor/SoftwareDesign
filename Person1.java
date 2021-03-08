6dbedfc7dc5fcc7c46fc6df0acb4d0d7eaa974f5
KybenNabor <79893978+kybenabor@users.noreply.github.com>
2021-03-08 18:53:29 +0800

    Create Person1.java
    That's great

---
+++ Person1.java
@@ -0,0 +1,48 @@
+   Public class Person 
+  {
+   private String Firstname;
+   private String Lastname;
+   
+   public String getName() {
+   return Firstname;
+   }
+   public String getName() {
+   return Lastname;
+   
+   public class Person {
+   String Firstname;
+   String Lastname;
+     
+   public static coid main(String[] args)
+   {
+   Person p = new Person();
+     
+   p.setFirstname("Kyben");
+   p.setLastname("Nabor");
+   System.out.println(p.getFirstname());
+   System.out.println(p.getLastname());
+     
+  p.printDetails();
+  }
+  public void setFirstName(String Firstname)
+  {
+  this.firstname=firstname;
+  }
+  public void setLastName(String Lastname)
+  {
+  this.Lastname=Lastname;
+  }
+  public String getFirstName() {
+  return this.Firstname;
+  }
+  public String getLastName() {
+  return this.Lastname;
+  }
+  public void printDetails() {
+  System.out.println(getFirstname + " " +getLastname);
+  }
+  }
+   }
+   }
+    
+   
